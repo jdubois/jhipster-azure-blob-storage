8e7ccd6ac26a44641b72aadf35f5d486bd3b1e1b
Julien Dubois <julien.dubois@gmail.com>
2019-09-03 23:36:50 +0200

    Unsecure the test URL

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -70,6 +70,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .authorizeRequests()
             .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/picture").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
